45604f9e34d36cb97b178886629ec45631de74a8
nison79 <georgenison@gmail.com>
2020-09-27 22:17:51 +0300

    animate colors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const tl = gsap.timeline({defaults: { ease:"power1.out" } });
 
-tl.to(".text", {y:"0%",duration:1, stagger:0.25});
-tl.to(".slider", {y:"-100%", duration: 1 , delay:1});
+tl.to(".text", {y:"0%",duration:2, stagger:0.2});
+tl.to(".slider", {y:"-100%", duration: 1 , delay:0.2});
 tl.to(".intro" , {y:"-100%" , opacity:0, duration : 1}, "-=1");
 tl.fromTo("nav", {opacity :0} , {opacity:1, duration:1});
 tl.fromTo(".big-text", {opacity :0} , {opacity:1, duration:1},"-=1");
 tl.fromTo(".landing", {opacity :0} , {opacity:1, duration:1},"-=2");
+
+TweenMax.to("html",3,{"--myColor":"black", delay:4.5})
